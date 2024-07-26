9797e206956dc34ecf8407c9807460ba5ea13a46
SebuBergman <basti.bergman@gmail.com>
2024-07-26 21:16:19 +0300

    just a simple api request nothing serius

--- apiRequest.js
+++ apiRequest.js
@@ -1,73 +1,58 @@
-const apiRequestsTemplate = document.createElement("template");
-apiRequestsTemplate.innerHTML = `
-<style>
-.mailSidebar__mailbox__button__div, .mailSidebar__mailbox__button {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 10px;
-    padding-right: 30px;
-}
 
-.mailSidebar__mailbox__button__heading {
-    padding-left: 10px;
-}
-
-</style>
-    <div class="apiRequest_Card">
-        <slot name="icon"></slot>
-        <h4 class="apiRequest_Card__heading"></h4>
-        <p class="apiRequest_Card__text"></p>
-        <img class="apiRequest_Card__artwork">
-    </div>
-    <p></p>
-`;
-
-class ApiRequest extends HTMLElement {
-    constructor() {
-        super();
-
-        this.attachShadow({ mode: "open" });  
-    }
-
-    connectedCallback() {
-        this.shadowRoot.append(apiRequestsTemplate.content.cloneNode(true));
-
-        const responseData = [];
-
-        let urls = [
-            'https://api.artic.edu/api/v1/artworks/129883',
-            'https://api.artic.edu/api/v1/artworks/129884',
-            'https://api.artic.edu/api/v1/artworks/129885',
-        ];
-
-        const fetchPromises = urls.map(url => fetch(url).then(response => response.json()));
-
-        async function getData() {
-            Promise.all(fetchPromises)
-            .then(responses => {
-                responseData.push(responses.map(response => response));
-                //console.log(responseData);
-                //console.log(responseData[0].data);
-            })
-            .catch(error => console.error('Error fetching data:', error)); 
-        }
-
+const displayedData = [
+    id = "",
+    title = "",
+    desc = "",
+    image = "",
+];
         
-        getData();
-        console.log(responseData)
-        
-        //const title = responseData[0].data;
-        //this.shadowRoot.querySelector("h4").innerText = title;
-        const unreadCount = this.getAttribute("unreadCount");
-        this.shadowRoot.querySelector("p").innerText = unreadCount;
-    }
+const urlsToFetch = [
+'https://api.artic.edu/api/v1/artworks/129883',
+'https://api.artic.edu/api/v1/artworks/129884',
+'https://api.artic.edu/api/v1/artworks/129885',
+];
 
-    disconnectedCallback() {
-        console.log("Our element is removed from DOM");
-        this.shadowRoot.querySelector("button").removeEventListener;
-    }
+// Function to fetch all of the URLs in parallel
+const fetchURLs = async (urls) => {
+try {
+const promises =
+    urls.map(url => fetch(url));
+
+// Wait for all of the promises to resolve
+const responses =
+    await Promise.all(promises);
+
+// Extract JSON data from responses
+const data = await
+    Promise.all(responses.map(response => response.json()));
+
+return data
+}
+catch (error) {
+throw new Error(`Failed to fetch data: ${error}`)
+}
 }
 
-window.customElements.define("app-api-content", ApiRequest);
+fetchURLs(urlsToFetch)
+.then(data => {
+data.forEach(painting => {
+    const markup = `<ul class="api__container" >
+                        <li><h3>${painting.data.title}</h3></li>
+                        <li><img src="https://www.artic.edu/iiif/2/${painting.data.image_id}/full/843,/0/default.jpg" class="api-image" /></li>
+                        <li>About: ${painting.data.thumbnail.alt_text}</li>
+                        <li>Painting by: ${painting.data.artist_title}</li>
+                    </ul>`;
+
+    document.querySelector("paintings").insertAdjacentHTML("beforeend", markup);
+})
+console.log('Fetched data:', data)
+/*for (let i = 0; i < data.length; i++) {
+    console.log(data.length);
+    displayedData.push(data[i].data.department_title);
+}*/
+})    
+.catch(error => {
+console.error('Error fetching data:', error)
+});
+
+console.log(displayedData);
